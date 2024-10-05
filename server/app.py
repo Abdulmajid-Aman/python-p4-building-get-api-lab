f4f44da2d4d5cb1a15fa3ea3d2e20a8b7412e50d
Abdulmajid Aman Hussein <abdulmajid.aman.student@moringaschool.com>
2024-10-05 19:55:08 +0300

    "Implemented API endpoints for bakeries, bakery by ID, baked goods by price, and most expensive baked good"

--- server/app.py
+++ server/app.py
@@ -20,19 +20,29 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    response = [bakery.to_dict() for bakery in Bakery.query.all()]
+
+    return make_response(response, 200)
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter(Bakery.id == id).first()
+
+    response = bakery.to_dict()
+    return make_response(response, 200)
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    response = [baked_good.to_dict() for baked_good in BakedGood.query.order_by(BakedGood.price.desc()).all()]
+
+    return make_response(response, 200)
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    item = BakedGood.query.order_by(BakedGood.price.desc()).first()
+
+    response = item.to_dict()
+    return make_response(response, 200)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
